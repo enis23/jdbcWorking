8e047d5d5c9e4032d339d4293d318d7ec8f16196
TechnoStudy <info@techno.study>
2020-01-25 15:28:04 -0500

    formatting

--- src/test/java/NewTestCases.java
+++ src/test/java/NewTestCases.java
@@ -13,7 +13,7 @@ public class NewTestCases {
         String url = "jdbc:mysql://database-techno.c771qxmldhez.us-east-2.rds.amazonaws.com:3306/daulet2030_studens_database";
         String user = "daulet2030";
         String password = "daulet2030@gmail.com";
-        connection = DriverManager.getConnection(url, user, password);
+        connection = DriverManager.getConnection( url, user, password );
         statement = connection.createStatement();
     }
 
@@ -23,28 +23,29 @@ public class NewTestCases {
     }
 
     @Test
-    public void  test() throws SQLException {
-        ResultSet resultSet = statement.executeQuery("select " +
+    public void test() throws SQLException {
+        ResultSet resultSet = statement.executeQuery( "select " +
                 "concat(first_name, ' ', last_name), country, city, postal_code " +
-                "from students");
-        while(resultSet.next()){
+                "from students" );
+        while(resultSet.next()) {
             String name = resultSet.getString( 1 );
             String country = resultSet.getString( 2 );
             String city = resultSet.getString( 3 );
             String postalCode = resultSet.getString( 4 );
-            System.out.println( name + ", "+country+ ", "+city+ ", "+postalCode);
+            System.out.println( name + ", " + country + ", " + city + ", " + postalCode );
         }
     }
+
     @Test
-    public void  testNew() throws SQLException {
-        ResultSet resultSet = statement.executeQuery("select" +
+    public void testNew() throws SQLException {
+        ResultSet resultSet = statement.executeQuery( "select" +
                 " * " +
-                "from students order by fee DESC limit 20");
+                "from students order by fee DESC limit 20" );
         while(resultSet.next()) {
             String email = resultSet.getString( "email" );
             Double fee = resultSet.getDouble( "fee" );
             String currency = resultSet.getString( "currency" );
-            System.out.println("email: " + email+", fee:" + fee + " " + currency );
+            System.out.println( "email: " + email + ", fee:" + fee + " " + currency );
         }
-        }
+    }
 }
