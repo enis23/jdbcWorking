1ea9cc2e9bf19a9533dad578f594facc2dd43b80
TechnoStudy <info@techno.study>
2020-01-25 14:45:14 -0500

    task1 and 2

---
+++ src/test/java/NewTestCases.java
@@ -0,0 +1,38 @@
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.sql.*;
+
+public class NewTestCases {
+    private Statement statement;
+    private Connection connection;
+
+    @BeforeClass
+    public void connect() throws SQLException {
+        String url = "jdbc:mysql://database-techno.c771qxmldhez.us-east-2.rds.amazonaws.com:3306/daulet2030_studens_database";
+        String user = "daulet2030";
+        String password = "daulet2030@gmail.com";
+        connection = DriverManager.getConnection(url, user, password);
+        statement = connection.createStatement();
+    }
+
+    @AfterClass
+    public void disconnect() throws SQLException {
+        connection.close();
+    }
+
+    @Test
+    public void  test() throws SQLException {
+        ResultSet resultSet = statement.executeQuery("select " +
+                "concat(first_name, ' ', last_name), country, city, postal_code " +
+                "from students");
+        while(resultSet.next()){
+            String name = resultSet.getString( 1 );
+            String country = resultSet.getString( 2 );
+            String city = resultSet.getString( 3 );
+            String postalCode = resultSet.getString( 4 );
+            System.out.println( name + ", "+country+ ", "+city+ ", "+postalCode);
+        }
+    }
+}
