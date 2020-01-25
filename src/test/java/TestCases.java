dc100b64a415453b2449bf03faed2d6ea5a63768
TechnoStudy <info@techno.study>
2020-01-25 12:07:49 -0500

    connect and disconnect

--- src/test/java/TestCases.java
+++ src/test/java/TestCases.java
@@ -1,15 +1,29 @@
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.sql.*;
 
 public class TestCases {
-    @Test
-    public void  test() throws SQLException {
+
+    private Statement statement;
+    private Connection connection;
+
+    @BeforeClass
+    public void connect() throws SQLException {
         String url = "jdbc:mysql://database-techno.c771qxmldhez.us-east-2.rds.amazonaws.com:3306/daulet2030_studens_database";
         String user = "daulet2030";
         String password = "daulet2030@gmail.com";
-        Connection connection = DriverManager.getConnection(url, user, password);
-        Statement statement = connection.createStatement();
+        connection = DriverManager.getConnection(url, user, password);
+        statement = connection.createStatement();
+    }
+
+    @AfterClass
+    public void disconnect() throws SQLException {
+        connection.close();
+    }
+    @Test
+    public void  test() throws SQLException {
         ResultSet rs= statement.executeQuery("SELECT first_name, last_name, email FROM students;");
         while (rs.next()){
             String name =rs.getString(1);
